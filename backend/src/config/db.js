4ec9e9c9e222dc9323426407817879fc54f1c244
Nacho Miranda <nachomirnd@gmail.com>
2026-05-04 12:40:58 -0300

    feat: Await para la validacion de DB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function conectDB() {
   try {
     const DB_URI = process.env.MONGODB_URI;
-    mongoose.connect(DB_URI);
+    await mongoose.connect(DB_URI);
     console.log("Successful MongoDB connection");
   } catch (err) {
     console.error("The connection with MongoDB failed.", err);
